b63682c90551f799ec72250f7a5b86af483195f1
Ibrahim Mert Akbas <akbas.ibrahim641@gmail.com>
2025-02-16 23:29:27 +0300

    Güncelleme işlemleri tamamlandı.

--- 2DongulerleUygulamalar.py
+++ 2DongulerleUygulamalar.py
@@ -1,3 +1,19 @@
+#✅ Görev 2: Kullanıcıdan bir sayı alarak 1’den o sayıya kadar olan sayıların 
+#toplamını hesaplayan bir Python programı yazın. (For veya While döngüsü kullanın.)
+
+sayi = int(input("Pozitif bir sayi giriniz: "))
+toplam=0
+#range sayi'ya kadar olan sayilari alir, sayinin kendisini dahil etmez. Sayi 5 olursa 5'i toplamaya dahil etmez.
+for i in range(sayi + 1):
+    toplam+=i
+print("Sayilarin toplami: ",toplam)
+
+#✅ Görev 3: 1 ile 100 arasındaki çift sayıları ekrana yazdıran bir Python programı yazın.
+
+#range içerisinde virgülden önceki sayi hangi sayi ile baslayacagımızı soyler.
+for i in range(1,100):
+    if i%2==0:
+        print(i)
 
 
 #✅ Görev 4: Kullanıcıdan alınan bir metni ters çeviren ve
